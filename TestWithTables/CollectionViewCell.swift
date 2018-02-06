eaa0bead15eecbd78b3da00204e0d7d3b8379f70
POST, LAURA <787598@ehs-s-232-30.local>
2018-02-06 11:35:12 -0600

    Collection view added

---
+++ TestWithTables/CollectionViewCell.swift
@@ -0,0 +1,15 @@
+//
+//  CollectionViewCell.swift
+//  Disco3
+//
+//  Created by POST, LAURA on 1/9/18.
+//  Copyright © 2018 Laura Post. All rights reserved.
+//
+
+import UIKit
+
+class CollectionViewCell: UICollectionViewCell {
+    @IBOutlet weak var studentLabel: UILabel!
+    
+    
+}
---
+++ TestWithTables/CollectionViewController.swift
@@ -0,0 +1,51 @@
+//
+//  CollectionViewController.swift
+//  Disco3
+//
+//  Created by POST, LAURA on 1/9/18.
+//  Copyright © 2018 Laura Post. All rights reserved.
+//
+
+import UIKit
+
+class CollectionViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate {
+
+    //MARK: Properties
+    let items = ["Student 1", "Student 2", "Student 3", "Student 4", "Student 5", "Student 6", "Student 7", "Student 8", "Student 9", "Student 10", "Student 11", "Student 12", "Student 13", "Student 14", "Student 15", "Student 16", "Student 17", "Student 18", "Student 18", "Student 19", "Student 20", "Student 21", "Student 22", "Student 23", "Student 24", "Student 25"]
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return items.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "CollectionCell", for: indexPath) as? CollectionViewCell
+        cell?.studentLabel.text = items[indexPath.item]
+        return cell!
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        print(indexPath.item)
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
